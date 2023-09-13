93c0a7fbb574840f370d7dce5c5723b4509c8ad2
rae charles <r_charles87@yahoo.com>
2023-09-13 19:42:10 -0400

    removed /test endpoint. endpoint was only for testing and not use by any clients.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 // Import express library
 const express = require("express");
 const cors = require("cors");
-const db = require('./db')
 
 // Import OUR stuff (our files, our components)
 const studentsController = require("./controllers/studentsController");
@@ -28,18 +27,6 @@ app.use("/v2/students", studentsControllerV2);
 // Healthcheck route
 app.get("/", (request, response) => {
   response.status(200).json({ data: "Service is running" });
-});
-
-//remove when finished 
-app.get('/tests', async (request, response) => {
-  try {
-    const tests = await db.any('SELECT * FROM tests;');
-
-
-    response.status(200).json({ data: tests });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
 });
 
 
